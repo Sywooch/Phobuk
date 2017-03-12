1cc7f13b6b21acbedca1490bc69fd6ec823c1f8c
Ewa Sieniawska <ewa.m.sieniawska@gmail.com>
2017-03-12 17:05:53 +0100

    Utworzenie widoku dla postów

---
+++ backend/views/post/view.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Post */
+
+$this->title = $model->id;
+$this->params['breadcrumbs'][] = ['label' => 'Posty', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="post-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Aktualizuj', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Usuń', ['delete', 'id' => $model->id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => 'Jesteś pewny, że chcesz usunąć ten post?',
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'id',
+            'text:ntext',
+            'user_id',
+            'category_id',
+            'photo_id',
+            'created_at',
+            'update_at',
+        ],
+    ]) ?>
+
+</div>
