ada33f6f8d52f72f020c33d9f984ad7a4c852ba2
Ewa Sieniawska <ewa.m.sieniawska@gmail.com>
2017-03-12 17:10:10 +0100

    Uworzenie widoku dla postów

---
+++ frontend/views/post/_form.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Post */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="post-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'text')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'category_id')->dropDownList(\yii\helpers\ArrayHelper::map(\common\models\Category::find()->all(), 'id', 'name'), ['prompt' => 'Wybierz kategorię']) ?>
+
+    <?= $form->field($model, 'photo_id')->textInput() ?>
+
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Utwórz' : 'Aktualizuj', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
