15ce69e322e03ec6a7c31d500622f36a66ae8c95
Ewa Sieniawska <ewa.m.sieniawska@gmail.com>
2017-03-12 17:02:19 +0100

    Utworzenie widoku dla komentarzy

---
+++ backend/views/comment/index.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\grid\GridView;
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $searchModel common\models\CommentSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Komentarze';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="comment-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Utwórz komentarz', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+
+            'text:ntext',
+            'created_at',
+            'user_id',
+            'photo_id',
+            // 'post_id',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
---
+++ backend/views/comment/view.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Comment */
+
+$this->title = $model->id;
+$this->params['breadcrumbs'][] = ['label' => 'Komentarze', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="comment-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Aktualizuj', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Usuń', ['delete', 'id' => $model->id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => 'Jesteś pewien, że chcesz usunąć ten komentarz?',
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'text:ntext',
+            'created_at',
+            'user_id',
+            'photo_id',
+            'post_id',
+        ],
+    ]) ?>
+
+</div>
