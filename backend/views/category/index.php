6efd57cd22f000c875cce7a46b330f50a09b6d84
Ewa Sieniawska <ewa.m.sieniawska@gmail.com>
2017-03-12 17:01:54 +0100

    Utworzenie widoku dla kategorii

---
+++ backend/views/category/index.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\grid\GridView;
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $searchModel common\models\CategorySearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Kategorie';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="category-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Utwórz kategorię', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'name',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
---
+++ backend/views/category/view.php
@@ -0,0 +1,36 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Category */
+
+$this->title = $model->name;
+$this->params['breadcrumbs'][] = ['label' => 'Kategorie', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="category-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Aktualizuj', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Usuń', ['delete', 'id' => $model->id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => 'Jesteś pewien, że chcesz usunąć ten rekord?',
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'name',
+            'id',
+        ],
+    ]) ?>
+
+</div>
