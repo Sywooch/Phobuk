70f3024791be43929eef1706c17dd73850344e56
Ewa Sieniawska <ewa.m.sieniawska@gmail.com>
2017-03-12 17:09:43 +0100

    Uworzenie widoku dla photo

---
+++ frontend/views/photo/_form.php
@@ -0,0 +1,32 @@
+<?php
+
+use common\models\Category;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Photo */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="photo-form">
+
+    <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]); ?>
+
+    <?= $form->errorSummary($model) ?>
+
+    <?= $form->field($model, 'imageFile')->fileInput() ?>
+
+    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'category_id')->dropDownList(ArrayHelper::map(Category::find()->all(), 'id', 'name'), ['prompt' => 'Wybierz kategorię']) ?>
+
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Dodaj' : 'Aktualizuj', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ frontend/views/photo/view.php
@@ -0,0 +1,47 @@
+<?php
+
+use common\models\Photo;
+use rmrevin\yii\fontawesome\FA;
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Photo */
+
+$this->title = $model->title;
+$this->params['breadcrumbs'][] = ['label' => 'Zdjęcia', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="photo-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <div class="row">
+
+
+        <?= FA::icon('calendar'); ?>
+        <?= Yii::$app->formatter->asDatetime($model->created_at) ?>
+
+        <?= Html::a(FA::icon('user') . ' ' . $model->user->username, ['/profile/'], [
+            'class' => 'btn btn-default btn-sm'
+        ]) ?>
+
+        <?= Html::a('#' . $model->category->name, ['/category/', 'id' => $model->id], [
+            'class' => 'btn btn-default btn-sm'
+        ]) ?>
+        <div class="btn-group pull-right" role="group">
+            <?= Html::a(FA::icon('pencil'), ['/photo/update', 'id' => $model->id], [
+                'class' => 'btn btn-default btn-sm'
+            ]) ?>
+            <?= Html::a(FA::icon('trash'), ['delete', 'id' => $model->id],
+                ['class' => 'btn btn-default btn-sm', 'data' => [
+                    'confirm' => 'Jesteś pewien, że chcesz usunąć to zdjęcie?',
+                    'method' => 'post',
+                ],
+                ]) ?>
+        </div>
+    </div>
+    <br>
+    <?= Html::img('/' . $model->photo, ['width' => '50%', 'height' => '50%', 'class' => 'center-block']); ?>
+
+
+</div>
