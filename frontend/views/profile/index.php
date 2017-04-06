5acba0b307c2489e9d6fc002205613ba24721d25
Ewa Sieniawska <ewa.m.sieniawska@gmail.com>
2017-04-06 09:43:44 +0200

    Profil użytkownika z wyświetlaniem zdjęć dodanych przez danego użytkownika

--- frontend/views/profile/index.php
+++ frontend/views/profile/index.php
@@ -3,11 +3,15 @@
  *
  * @var $user \common\models\User
  * @var $city \common\models\City
+ * @var $avatar \common\models\Photo
  * @var $isCurrentUser bool
  * @var $count
+ * @var $photoDataProvider
  * rmrevin\yii\fontawesome\AssetBundle::register($this);
  */
 use rmrevin\yii\fontawesome\FA;
+use yii\helpers\Html;
+use yii\widgets\ListView;
 
 $this->title = $user->getFullName();
 ?>
@@ -15,13 +19,13 @@ $this->title = $user->getFullName();
     <div class="container padding-wrapper-fix ">
         <div class="row">
             <div class="col-xs-4 col-sm-3 col-md-3 col-sm-offset-3 col-md-offset-3  ">
-                <p>Tu jest piękne zdjęcie profilowe</p>
+                <?= Html::img('/' . $avatar->photo, ['class' => ' img-responsive avatar']); ?>
             </div>
             <div class="col-xs-4 col-sm-3 col-md-3 ">
-                <div class="row">
+                <div class="row" style="margin-bottom: 0%">
                     <h3><?= $user->getFullName() ?></h3>
                 </div>
-                <div class="row">
+                <div class="row" style="margin-bottom: 0%">
                     <h5><?= $user->getUsername() ?></h5>
                 </div>
                 <div class="row">
@@ -32,20 +36,42 @@ $this->title = $user->getFullName();
                     <?= FA::icon('signal') ?>
                     <?= $user->getLevelLabel() ?>
                 </div>
+                <div class="row">
+                    <p> <?= FA::icon('photo') ?>
+                        Ilość zdjęć:
+                        <?= $count ?> </p>
+
+                </div>
             </div>
 
 
             <div class="col-xs-4 col-sm-3 col-md-3 ">
-                <p>3tu jest tekst</p>
+                <div class="row">
+                    <?= Html::a('Znajomi', ['/friendship/'], ['class' => 'btn btn-primary btn-color']) ?>
+                </div>
+                <div class="row">
+                    <?= Html::a('Aktualizuj profil', ['update', 'id' => $user->id], ['class' => 'btn btn-primary btn-color']) ?>
+                </div>
+                <div class="row">
+                    <?= Html::a('Dodaj zdjęcie', ['/photo/create'], ['class' => 'btn btn-primary btn-color']) ?>
+                </div>
+                <div class="row">
+                    <?= Html::a('Dodaj post', ['/post/create'], ['class' => 'btn btn-primary btn-color']) ?>
+                </div>
             </div>
         </div>
     </div>
 </div>
 
-<div class="container">
-    <h1><?= $user->username ?></h1>
-    <h1><?= $city->name ?></h1>
+<div class="container ">
+    <div class="row">
 
-
+        <?= ListView::widget([
+            'dataProvider' => $photoDataProvider,
+            'itemView' => '_photo',
+            'viewParams' => ['avatar' => $avatar],
+            'summary' => '',
+        ]);
+        ?>
 </div>
-
+</div>
