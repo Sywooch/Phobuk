1ebd3058c055fb08feb103a432f1af23d6c8a8d2
Ewa Sieniawska <ewa.m.sieniawska@gmail.com>
2017-04-21 09:42:15 +0200

    Wyświetlanie zdjęć użytkownika za pomocą widgetu PhotoItemWidget

--- frontend/views/profile/_photo.php
+++ frontend/views/profile/_photo.php
@@ -1,50 +1,10 @@
 <?php
 
 
-use rmrevin\yii\fontawesome\FA;
-use yii\helpers\Html;
+use common\widgets\PhotoItemWidget\PhotoItemWidget;
 
 /** @var \common\models\Photo $model */
-/* @var $avatar \common\models\Photo */
 
 ?>
-
-
-<div class="col-sm-6  ">
-
-
-    <div class="thumbnail thumbnail-color">
-
-        <div class="caption">
-            <h3 style="text-align: center"><?= $model->title ?>
-            </h3>
-        </div>
-        <div class="row">
-            <div class="col-xs-6 col-sm-4 col-md-4 ">
-                <?= Html::img('/' . $avatar->photo, ['class' => ' img-responsive avatar-small']); ?>
-            </div>
-
-            <div class="col-xs-4 col-sm-8 col-md-8 ">
-                <div class="row">
-                    <?= $model->user->getFullName() ?>
-                </div>
-                <div class="row ">
-                    <?= FA::icon('calendar'); ?>
-                    <?= Yii::$app->formatter->asDate($model->created_at) ?>
-                </div>
-            </div>
-        </div>
-
-        <div class="row" style="margin-left: 2%">
-            <?= Html::a('#' . $model->category->name, ['/category/', 'id' => $model->id], [
-                'class' => 'btn btn-default btn-sm']) ?>
-        </div>
-
-        <div class="caption">
-            <?= Html::img('/' . $model->photo, ['class' => ' img-responsive card-img img-fluid ']); ?>
-        </div>
-    </div>
-
-
-</div>
+<?= PhotoItemWidget::widget(['model' => $model]) ?>
 
