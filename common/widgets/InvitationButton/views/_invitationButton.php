b5b14c716bf4c754ed944310269856cd63db3290
Ewa Sieniawska <ewa.m.sieniawska@gmail.com>
2017-04-25 16:07:29 +0200

    Dodanie informacji o braku zaproszeń do znajomych

--- common/widgets/InvitationButton/views/_invitationButton.php
+++ common/widgets/InvitationButton/views/_invitationButton.php
@@ -17,19 +17,25 @@ use yii\helpers\Html;
 <div class='navbar-form navbar-inverse navbar-right dropdown'>
     <div class='dropdown dropdown-toggle' data-toggle='dropdown' aria-expanded='false'>
         <div class="invited"> <?= FA::icon('user-plus') ?> </div>
+        <?php if ($friendship->count > 0) { ?>
         <div class="label-info"> <?= $friendship->count ?></div>
-
+        <?php } ?>
         <span class='caret'></span>
     </div>
     <div class='dropdown-menu dropdown-messages'>
-        <?php foreach ($friendship->models as $model) {
+        <?php if ($friendship->count == 0) { ?>
+            <p>Nie masz zaproszeń do znajomych</p>
+        <?php }
+        foreach ($friendship->models as $model) {
             $query = User::findOne($model->friend_one);
             $fullName = $query->getFullName();
             ?>
             <div class="row">
                 <div class="col-xs-12 dropdown-messages-text">
                     <p><?= Html::a($fullName, ['/profile', 'id' => $model->friend_one]) ?></p>
-                    <p><?= $model->created_at ?></p></div>
+                    <p><?= $model->created_at ?></p>
+
+                </div>
                 <div class="col-xs-6">
                     <?= Html::a('Potwierdź', ['friendship/confirm-invite', 'id' => $model->friend_one], ['class' => 'btn btn-primary btn-color']); ?>
                 </div>
@@ -40,3 +46,4 @@ use yii\helpers\Html;
             <div class='divider'></div>
         <?php } ?>
     </div>
+</div>
