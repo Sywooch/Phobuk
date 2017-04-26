7fd672f49477e4d134018312793ebd681f8033e0
Ewa Sieniawska <ewa.m.sieniawska@gmail.com>
2017-04-26 20:20:55 +0200

    Utworzenie PostCommentControllera

---
+++ frontend/controllers/PostCommentController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace frontend\controllers;
+
+use common\models\PostComment;
+use common\models\PostCommentSearch;
+use Yii;
+use yii\filters\VerbFilter;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+
+/**
+ * PostCommentController implements the CRUD actions for PostComment model.
+ */
+class PostCommentController extends Controller {
+    /**
+     * @inheritdoc
+     */
+    public function behaviors() {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all PostComment models.
+     * @return mixed
+     */
+    public function actionIndex() {
+        $searchModel = new PostCommentSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Creates a new PostComment model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate($id) {
+        $model = new PostComment();
+
+        $model->load(Yii::$app->request->post());
+        $model->post_id = $id;
+
+        if ($model->save()) {
+            return $this->redirect(Yii::$app->request->referrer);
+        } else {
+            var_dump($model->attributes);
+        }
+    }
+
+    /**
+     * Deletes an existing PostComment model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionDelete($id) {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(Yii::$app->request->referrer);
+    }
+
+    /**
+     * Finds the PostComment model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return PostComment the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id) {
+        if (($model = PostComment::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+}
