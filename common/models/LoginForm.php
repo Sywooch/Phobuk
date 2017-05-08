1c9f7ca2c47f1ade8143a272ace5c8edec1abd0a
Ewa Sieniawska <ewa.m.sieniawska@gmail.com>
2017-05-08 12:03:01 +0200

    Przetłumaczenie na język polski nazw

--- common/models/LoginForm.php
+++ common/models/LoginForm.php
@@ -31,6 +31,16 @@ class LoginForm extends Model
         ];
     }
 
+
+    public function attributeLabels() {
+        return [
+            'username' => 'Nazwa użytkownika',
+            'rememberMe' => 'Pamiętaj mnie',
+            'password' => 'Hasło',
+
+
+        ];
+    }
     /**
      * Validates the password.
      * This method serves as the inline validation for password.
@@ -43,7 +53,7 @@ class LoginForm extends Model
         if (!$this->hasErrors()) {
             $user = $this->getUser();
             if (!$user || !$user->validatePassword($this->password)) {
-                $this->addError($attribute, 'Incorrect username or password.');
+                $this->addError($attribute, 'Nieprawidłowa nazwa użytkownika lub hasło.');
             }
         }
     }
