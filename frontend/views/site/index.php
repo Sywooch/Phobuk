f3e4ccf286eb9e610306869d6d6b62526c515f78
Ewa Sieniawska <ewa.m.sieniawska@gmail.com>
2017-05-08 21:12:45 +0200

    Usunięcie tytułu strony

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,14 +1,12 @@
 <?php
 
 /* @var $this yii\web\View */
-use yii\helpers\Html;
 use yii\widgets\ListView;
 
 /* @var $photoFriendProvider yii\data\ActiveDataProvider */
 
 $this->title = Yii::$app->name;
 ?>
-<h1><?= Html::encode($this->title) ?></h1>
 <div class="site-index">
 
 
