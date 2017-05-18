4daeca7f6e19b80f6a1d97cede202d623a8613d5
Ewa Sieniawska <ewa.m.sieniawska@gmail.com>
2017-05-18 18:30:46 +0200

    Zmiana wyświetlania kategorii

--- backend/views/category/index.php
+++ backend/views/category/index.php
@@ -8,12 +8,10 @@ use yii\helpers\Html;
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
 $this->title = 'Kategorie';
-$this->params['breadcrumbs'][] = $this->title;
+
 ?>
 <div class="category-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
         <?= Html::a('Utwórz kategorię', ['create'], ['class' => 'btn btn-success']) ?>
